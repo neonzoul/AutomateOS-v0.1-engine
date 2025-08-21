a80671731eb17a669f984bd1b6ffce32903de9d5
Your Name <indystyle33@gmail.com>
2025-08-21 10:46:56 +0700

    feat: add webhook endpoint to trigger workflows

---
+++ app/api/v1/endpoints/webhook.py
@@ -0,0 +1,27 @@
+# Webhook endpoint for handle webhooks logic.
+
+from fastapi import APIRouter, status
+
+from app.core.queue import q
+
+router = APIRouter()
+
+@router.post("/{workflow_id}", status_code=status.HTTP_202_ACCEPTED)
+
+# --- Recieves Webhook request Endpoint --- 
+async def trigger_workflow(workflow_id: int):
+    """
+    [[[ This endpoint receives a webhook request, queues a job for background
+     processing, and return immediatly. ]]]
+    """
+    
+    q.enqueue("app.engine.tasks.process_workflow", workflow_id) # type: ignore
+    #[[ To ignore Pylance type error because rq uses dynamic strings for function paths. ]]
+
+    return {"message": "Workflow execution has been queued."}
+
+    """ [[[
+    - This is the only job of the API: add a task to the queue.
+    - 'process_workflow' is the name of the function the worker will run.
+    - args=(workflow_id,) are the arguments passes to that function.
+    ]]] """
