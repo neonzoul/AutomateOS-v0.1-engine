dbad94c2907b48205908b85589d6f116831552ea
Your Name <indystyle33@gmail.com>
2025-08-21 11:27:08 +0700

    feat: Implement process_workflow function for webhook connection

---
+++ app/engine/tasks.py
@@ -0,0 +1,9 @@
+def process_workflow(workflow_id: int):
+    """
+    Task for prove webhooks connection with trigger_workflow webhook.
+    """
+    print(f"Worker is processing workflow ID: {workflow_id}")
+    
+    # {To-do week4}
+
+    return f"Successfully processed workflow {workflow_id}"
